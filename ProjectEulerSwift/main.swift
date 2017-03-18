d31caa20d911599d4c2c67c126d84c9fc3440f85
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-18 21:02:47 +0900

    ADD Problem 004

--- ProjectEulerSwift/main.swift
+++ ProjectEulerSwift/main.swift
@@ -2,3 +2,4 @@
 print(P1.answer())
 print(P2.answer())
 print(P3.answer())
+print(P4.answer())
---
+++ ProjectEulerSwift/p4.swift
@@ -0,0 +1,38 @@
+import Foundation
+extension ProjectEuler{
+    static func getMaxPalindromicNumber(digit: Int) -> Int{
+        if(digit < 2){
+            return 0
+        }
+        let maxValue = Int(pow(Double(10), Double(digit))) - 1
+        let minValue = Int(pow(Double(10), Double(digit - 1)))
+        var maxPalindromicNum = 0
+        var endValue = 0
+        
+        for_i: for i in (minValue...maxValue).reversed(){
+            for_j: for j in (minValue...maxValue).reversed(){
+                if(endValue > i && endValue > j){
+                    break for_i
+                }
+                if(isPalindromicNumber(value: i * j)){
+                    maxPalindromicNum = i * j > maxPalindromicNum ? i * j : maxPalindromicNum
+                    endValue = i < j ? i : j
+                    break for_j
+                }
+            }
+        }
+        return maxPalindromicNum
+    }
+    
+    static func isPalindromicNumber(value: Int) -> Bool{
+        let strValue = String(value)
+        let reverseValue = String(strValue.characters.reversed())
+        return strValue == reverseValue
+    }
+}
+
+class P4: ProjectEuler{
+    static func answer() -> Int {
+        return getMaxPalindromicNumber(digit: 3)
+    }
+}
