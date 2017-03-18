13c72bd5151bdeec54d283408817bda45dfd45bc
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-18 17:29:40 +0900

    CHANGE main.swift

--- ProjectEulerSwift/main.swift
+++ ProjectEulerSwift/main.swift
@@ -1,12 +1,4 @@
-//
-//  main.swift
-//  ProjectEulerSwift
-//
-//  Created by Yoshida Yusuke on 2017/03/18.
-//  Copyright © 2017年 Yoshida Yusuke. All rights reserved.
-//
 
-import Foundation
-
-print("Hello, World!")
-
+print(P1.answer())
+print(P2.answer())
+print(P3.answer())
