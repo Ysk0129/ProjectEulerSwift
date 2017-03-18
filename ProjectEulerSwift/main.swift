ad7de7dbae9cbee83585ad9591a26fb46524bc35
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-18 17:20:09 +0900

    Initial Commit

---
+++ ProjectEulerSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEulerSwift
+//
+//  Created by Yoshida Yusuke on 2017/03/18.
+//  Copyright © 2017年 Yoshida Yusuke. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
