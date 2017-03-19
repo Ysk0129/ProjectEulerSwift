4aa2ada8c50e6a9bbedf7d6710308283093d91bc
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-19 18:41:35 +0900

    UPDATE improved the efficiency of method "getPrimeFactors(value:) "

--- ProjectEulerSwift/p3.swift
+++ ProjectEulerSwift/p3.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 extension ProjectEuler{
     static func getMaxPrimeFactor(value: UInt64) -> Int{
         guard let maxPrimeFactor = getPrimeFactors(value: value).max() else {
@@ -10,13 +10,26 @@ extension ProjectEuler{
     static func getPrimeFactors(value: UInt64) -> [Int]{
         var factors = [Int]()
         var varValue: UInt64 = value
-        var divisor = 2
+        var divisor = 3
+        let sqrtValue = Int(sqrt(Double(value)))
+        
+        if(value <= 1){
+            return [0]
+        }
+        while(varValue % 2 == 0){
+            factors.append(2)
+            varValue /= UInt64(2)
+        }
         while(varValue > 1){
             if(varValue % UInt64(divisor) == 0){
                 factors.append(divisor)
                 varValue /= UInt64(divisor)
             }else{
-                divisor += 1
+                divisor += 2
+                if(divisor > sqrtValue){
+                    factors.append(Int(varValue))
+                    return factors
+                }
             }
         }
         return factors
