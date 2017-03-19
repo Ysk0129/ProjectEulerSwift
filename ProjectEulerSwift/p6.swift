0d260d0aef50c0306a4d70f7383eac3ebcaa22a9
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-19 18:45:51 +0900

    CLEAN delete unnecessary type declaration

--- ProjectEulerSwift/p6.swift
+++ ProjectEulerSwift/p6.swift
@@ -2,7 +2,7 @@ import Foundation
 extension ProjectEuler{
     static func diffSumSquares(number: Int) -> Int{
         let sumOfSquares = (1...number).map{$0 * $0}.reduce(0){$0 + $1}
-        let squareOfSum: Int = Int(pow(Double((1...number).reduce(0){$0 + $1}),2))
+        let squareOfSum = Int(pow(Double((1...number).reduce(0){$0 + $1}),2))
         return squareOfSum - sumOfSquares
     }
 }
