ac9695e0e8cfed19035fb46ddf6eb257078178e4
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-19 14:41:37 +0900

    ADD Problem 006

---
+++ ProjectEulerSwift/p6.swift
@@ -0,0 +1,14 @@
+import Foundation
+extension ProjectEuler{
+    static func diffSumSquares(number: Int) -> Int{
+        let sumOfSquares = (1...number).map{$0 * $0}.reduce(0){$0 + $1}
+        let squareOfSum: Int = Int(pow(Double((1...number).reduce(0){$0 + $1}),2))
+        return squareOfSum - sumOfSquares
+    }
+}
+
+class P6: ProjectEuler{
+    static func answer() -> Int {
+        return diffSumSquares(number: 100)
+    }
+}
