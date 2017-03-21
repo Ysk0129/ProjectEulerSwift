176a62edb8bc0f59050b4eac7f849400b24c084a
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-21 21:14:21 +0900

    CLEAN Delete unnecessary if condition

--- ProjectEulerSwift/p9.swift
+++ ProjectEulerSwift/p9.swift
@@ -15,7 +15,7 @@ extension ProjectEuler{
         // Substitute 2'. for 1.
         // (4). a^2 + b^2 = (sum - (a + b))^2
         // Since (2). and (3). conditions exist, (5). is established
-        // (5). 1 =< a < sum / 3, 2 =< b < sum / 2, sum / 3 + 1 =< c < sum - 3
+        // (4). 1 =< a < sum / 3, 2 =< b < sum / 2, sum / 3 + 1 =< c < sum - 3
         
         if(sum < 3){
             return nil
@@ -26,7 +26,7 @@ extension ProjectEuler{
         for a in 1..<sum / 3{
             for b in (a + 1)..<sum / 2{
                 let c = sum - (a + b)
-                if(a + b + c == sum && a * a + b * b == c * c){
+                if(a * a + b * b == c * c){
                     pythagoreanDict.append(["a": a, "b": b, "c": c])
                 }
             }
