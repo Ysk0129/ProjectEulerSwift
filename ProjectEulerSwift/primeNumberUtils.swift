955b3d5317a8b6e2288318dfb69e2e2d905c7c60
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-22 23:55:33 +0900

    CLEAN Move method on prime number

---
+++ ProjectEulerSwift/primeNumberUtils.swift
@@ -0,0 +1,45 @@
+import Foundation
+extension ProjectEuler{
+    static func getPrimeNumberOf(nth: Int) -> Int{
+        if(nth < 1){
+            return 0
+        }
+        if(nth == 1){
+            return 2
+        }
+        var remainingPrimeNum = nth - 1
+        var currentNum = 3
+        var currentPrimeNum = 3
+        
+        while(remainingPrimeNum > 0){
+            if(isPrimeNumber(value: currentNum)){
+                remainingPrimeNum -= 1
+                currentPrimeNum = currentNum
+            }
+            currentNum += 2
+        }
+        return currentPrimeNum
+    }
+    static func isPrimeNumber(value: Int) -> Bool{
+        if(value == 2){
+            return true
+        }
+        if(value <= 1 || value % 2 == 0){
+            return false
+        }
+        
+        let maxDivisor = Int(sqrt(Double(value)))
+        var currentDivisor = 3
+        
+        while(currentDivisor <= maxDivisor){
+            if(value % currentDivisor == 0){
+                if(value == currentDivisor){
+                    return true
+                }
+                return false
+            }
+            currentDivisor += 2
+        }
+        return true
+    }
+}
