918fc9c91baa0f3bc50aec9b1f6360df12ef7b20
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-25 01:34:22 +0900

    CLEAN Method "getPrimeFactors (value :)" can be reused

--- ProjectEulerSwift/p3.swift
+++ ProjectEulerSwift/p3.swift
@@ -6,34 +6,6 @@ extension ProjectEuler{
         }
         return maxPrimeFactor
     }
-    
-    static func getPrimeFactors(value: UInt64) -> [Int]{
-        var factors = [Int]()
-        var varValue: UInt64 = value
-        var divisor = 3
-        let sqrtValue = Int(sqrt(Double(value)))
-        
-        if(value <= 1){
-            return [0]
-        }
-        while(varValue % 2 == 0){
-            factors.append(2)
-            varValue /= UInt64(2)
-        }
-        while(varValue > 1){
-            if(varValue % UInt64(divisor) == 0){
-                factors.append(divisor)
-                varValue /= UInt64(divisor)
-            }else{
-                divisor += 2
-                if(divisor > sqrtValue){
-                    factors.append(Int(varValue))
-                    return factors
-                }
-            }
-        }
-        return factors
-    }
 }
 
 class P3: ProjectEuler{
--- ProjectEulerSwift/primeNumberUtils.swift
+++ ProjectEulerSwift/primeNumberUtils.swift
@@ -20,6 +20,7 @@ extension ProjectEuler{
         }
         return currentPrimeNum
     }
+    
     static func isPrimeNumber(value: Int) -> Bool{
         if(value == 2){
             return true
@@ -41,5 +42,33 @@ extension ProjectEuler{
             currentDivisor += 2
         }
         return true
+    }
+    
+    static func getPrimeFactors(value: UInt64) -> [Int]{
+        var factors = [Int]()
+        var varValue: UInt64 = value
+        var divisor = 3
+        let sqrtValue = Int(sqrt(Double(value)))
+        
+        if(value <= 1){
+            return [0]
+        }
+        while(varValue % 2 == 0){
+            factors.append(2)
+            varValue /= UInt64(2)
+        }
+        while(varValue > 1){
+            if(varValue % UInt64(divisor) == 0){
+                factors.append(divisor)
+                varValue /= UInt64(divisor)
+            }else{
+                divisor += 2
+                if(divisor > sqrtValue){
+                    factors.append(Int(varValue))
+                    return factors
+                }
+            }
+        }
+        return factors
     }
 }
