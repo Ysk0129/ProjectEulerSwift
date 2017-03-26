53adcbb128f3a60a9d6168ae0ee142bd33d25760
Yoshid01 <garlic.ba.0129@gmail.com>
2017-03-26 12:18:37 +0900

    CLEAN CLEAN Processing when the argument is 1 or less

--- ProjectEulerSwift/p6.swift
+++ ProjectEulerSwift/p6.swift
@@ -1,6 +1,9 @@
 import Foundation
 extension ProjectEuler{
     static func diffSumSquares(number: Int) -> Int{
+        if(number <= 1){
+            return 0
+        }
         let sumOfSquares = (1...number).map{$0 * $0}.reduce(0){$0 + $1}
         let squareOfSum = Int(pow(Double((1...number).reduce(0){$0 + $1}),2))
         return squareOfSum - sumOfSquares
