cb5bc9b5861aeb750d95ec16c50404605c3dc24f
Yoshid01 <garlic.ba.0129@gmail.com>
2017-04-07 19:01:06 +0900

    ADD Problem 024(Improvement required)

---
+++ ProjectEulerSwift/p24.swift
@@ -0,0 +1,45 @@
+
+class P24: ProjectEuler{
+
+    static func answer() -> Int{
+        var num = 0
+        var count = 0
+        let nums = ["0","1","2","3","4","5","6","7","8","9"]
+        //Todo:It should use a recursive function
+        for_a: for a in nums{
+            let B = nums.filter{$0 != a}
+            for b in B{
+                let C = B.filter{$0 != b}
+                for c in C{
+                    let D = C.filter{$0 != c}
+                    for d in D{
+                        let E = D.filter{$0 != d}
+                        for e in E{
+                            let F = E.filter{$0 != e}
+                            for f in F{
+                                let G = F.filter{$0 != f}
+                                for g in G{
+                                    let H = G.filter{$0 != g}
+                                    for h in H{
+                                        let I = H.filter{$0 != h}
+                                        for i in I{
+                                            let J = I.filter{$0 != i}
+                                            for j in J{
+                                                count += 1
+                                                if(count == 1000000){
+                                                    num = Int(a + b + c + d + e + f + g + h + i + j)!
+                                                    break for_a
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return num
+    }
+}
