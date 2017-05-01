9ff442d82b6c209e89dcf6454b42dfe784668391
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-02 00:59:02 +0900

    CHANGE Return  type to a tuple and set the return value to "what number" and "value"

--- ProjectEulerSwift/P025.swift
+++ ProjectEulerSwift/P025.swift
@@ -1,28 +1,35 @@
 
 extension ProjectEuler{
     
-    static func getFirstOfFibonacci(nDigit: Int) -> Int{
+    static func getFirstOfFibonacci(nDigit: Int) -> (nth: Int, value: String){
         if(nDigit <= 0){
-            return 0
+            return (nth:0, value: "0")
         }
+        if(nDigit == 1){
+            return (nth:1, value: "1")
+        }
+        
         var x = "0"
         var y = "1"
         var z = "1"
         var digitsCount = 0
         var count = 1
+        var value = "1"
         while(digitsCount < nDigit){
             z = bigAdd(n: x, m: y)
             x = y
             y = z
             digitsCount = z.characters.count
             count += 1
+            value = z
         }
-        return count
+        
+        return (nth: count, value: value)
     }
 }
 
 class P25: ProjectEuler{
     static func answer() -> Int{
-        return getFirstOfFibonacci(nDigit: 1000)
+        return getFirstOfFibonacci(nDigit: 1000).nth
     }
 }
